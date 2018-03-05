e9ef35a98a95e910398cf9b842c5f7f4f31f81bf
Rsantct <rsantct@gmail.com>
2018-03-05 17:19:58 +0100

    Update webcustombutton_8.sh

--- home/firtro/bin/webcustombutton_8.sh
+++ home/firtro/bin/webcustombutton_8.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Ajuste para Spotify (librespot)
+# Ajuste para Spotify (librespot) necesita un buffer grande
+# en maquinas lentas como RPI1
 
 /home/firtro/bin_custom/restart_jack.sh 8192
 /home/firtro/bin/control level_add 0
