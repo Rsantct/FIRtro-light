f387a890b471e9f4d24479aee68210c6770d65b1
Rsantct <rsantct@gmail.com>
2018-03-05 08:03:51 +0000

    Update webcustombutton_7.sh

--- home/firtro/bin/webcustombutton_7.sh
+++ home/firtro/bin/webcustombutton_7.sh
@@ -4,5 +4,5 @@
 # (es posible que provoque under runs con players que consumen %CPU)
 
 /home/firtro/bin_custom/restart_jack_brutefir_ecasound.sh 256
-
+/home/firtro/bin/control level_add 0
 /home/firtro/bin/control input analog
