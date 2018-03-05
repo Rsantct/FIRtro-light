d74107e7bc166a2a2529928e694e8b0d64d955a2
Rsantct <rsantct@gmail.com>
2018-03-05 17:01:06 +0100

    Update webcustombutton_7.sh

--- home/firtro/bin/webcustombutton_7.sh
+++ home/firtro/bin/webcustombutton_7.sh
@@ -3,6 +3,6 @@
 # Ajuste de baja latencia para la entrada analogica 
 # (es posible que provoque under runs con players que consumen %CPU)
 
-/home/firtro/bin_custom/restart_jack_brutefir_ecasound.sh 256
+/home/firtro/bin_custom/restart_jack.sh 256
 /home/firtro/bin/control level_add 0
 /home/firtro/bin/control input analog
