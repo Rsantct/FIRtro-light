83392f879ce69d403fc6a8686653881f7b32db6d
Rsantct <rsantct@gmail.com>
2018-03-10 12:51:32 +0100

    Update restart_jack.sh

--- home/firtro/bin_custom/restart_jack.sh
+++ home/firtro/bin_custom/restart_jack.sh
@@ -15,7 +15,7 @@
 # (se verifica que Brutefir partition size >= Jack buffer size)
 #
 # TO DO: esto empezó como un script sencillo en bash,
-#        pero habría que reescribirlo en python
+#        pero habría que reescribirlo en python :-/
 #
 
 home="/home/firtro"
