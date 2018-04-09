503ac75eb309cc36ad15f894dd0afefae44994c3
Rsantct <rsantct@gmail.com>
2018-04-10 00:10:31 +0200

    Update patch_capture_gain.sh

--- home/firtro/bin_custom/patch_capture_gain.sh
+++ home/firtro/bin_custom/patch_capture_gain.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+gaindB=0.0
+if [ $1 ]; then
+    gaindB=$1
+fi
+
 patched=$(grep capture_gain.sh /home/firtro/bin/initfirtro.py)
 
 if [[ $patched == "" ]]; then
     echo "(i) patcheando initfirtro.py"
-    echo "/home/firtro/bin_custom/capture_gain.sh" >> /home/firtro/bin/initfirtro.py
+    echo "/home/firtro/bin_custom/capture_gain.sh "$gaindB >> /home/firtro/bin/initfirtro.py
 else
     echo "(i) ya estaba patcheado initfirtro.py"
 fi
