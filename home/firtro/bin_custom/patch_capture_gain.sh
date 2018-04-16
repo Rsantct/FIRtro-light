5ee3801c0e41e8611131c1ec79a787e49d2aa20d
Rsantct <rsantct@gmail.com>
2018-04-16 06:51:25 +0000

    Update patch_capture_gain.sh

--- home/firtro/bin_custom/patch_capture_gain.sh
+++ home/firtro/bin_custom/patch_capture_gain.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 # Audioinjector I2S sound card
-# 
-# Se ha observado una sensibilidad escasa en la entrada analógica de esta tarjeta, 
+#
+# Se ha observado una sensibilidad escasa en la entrada analógica de esta tarjeta,
 # lo que supone que algunas fuentes como una TV pueden tener un nivel muy bajo.
-# 
-# Para ello podemos insertar la siguiente orden de patcheo antes de llamar a initfirtro.py 
+#
+# Para ello podemos insertar la siguiente orden de patcheo antes de llamar a initfirtro.py
 # en el archivo /etc/rc.local de arranque de la máquina:
-# 
+#
 #   /etc/rc.local
-# 
+#
 #     su -l firtro -c "/home/firtro/bin_custom/patch_capture_gain.sh 6.0"
 #     su -l firtro -c "/home/firtro/bin/initfirtro.py"
-# 
+#
 # Entonces debemos asociar la entrada analógica de FIRtro a los nuevos puertos que la amplifican:
-# 
+#
 #   audio/inputs
-# 
+#
 #     [analog]
 #     #in_ports:   system:capture_1 system:capture_2
 #     in_ports:    capture_gain:out_1 capture_gain:out_2
-# 
+#
 # La ganancia se puede reajustar ejecutando capture_gain.sh, por ejemplo:
-# 
+#
 #    $ ~/bin_custom/capture_gain.sh +3.0
-#    
+#
 
 gaindB=0.0
 if [ $1 ]; then
@@ -35,7 +35,14 @@ patched=$(grep capture_gain.sh /home/firtro/bin/initfirtro.py)
 
 if [[ $patched == "" ]]; then
     echo "(i) patcheando initfirtro.py"
-    echo "    Popen('/home/firtro/bin_custom/capture_gain.sh "$gaindB"', shell=True)" >> /home/firtro/bin/initfirtro.py
+    echo "" \
+        >> /home/firtro/bin/initfirtro.py
+    echo "    # Patch para disponer de un puerto Jack con una ganancia sobre system:capture" \
+        >> /home/firtro/bin/initfirtro.py
+    echo "    # https://github.com/Rsantct/FIRtro-light/wiki/205-sound-card-analog-input" \
+        >> /home/firtro/bin/initfirtro.py
+    echo   "    Popen('/home/firtro/bin_custom/capture_gain.sh "$gaindB"', shell=True)" \
+        >> /home/firtro/bin/initfirtro.py
 else
     echo "(i) ya estaba patcheado initfirtro.py"
 fi
