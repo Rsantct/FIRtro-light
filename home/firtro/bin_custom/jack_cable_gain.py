55314f34af1de0fa09ca56a0747dae5f5209d508
Rsantct <rsantct@gmail.com>
2018-04-19 09:48:03 +0000

    Update jack_cable_gain.py

--- home/firtro/bin_custom/jack_cable_gain.py
+++ home/firtro/bin_custom/jack_cable_gain.py
@@ -87,12 +87,12 @@ if __name__ == "__main__":
     # Los conectamos los puertos de captura y playback deseados:
     if source:
         if disconnect:
-            conecta(source, "cable_gain", mode=disconnect)
-        conecta(source, "cable_gain", mode=connect)
+            conecta(source, "cable_gain", mode='disconnect')
+        conecta(source, "cable_gain", mode='connect')
     if sink:
         if disconnect:
-            conecta("cable_gain", sink, mode=disconnect)
-        conecta("cable_gain", sink, mode=connect)
+            conecta("cable_gain", sink, mode='disconnec't)
+        conecta("cable_gain", sink, mode='connect')
 
     # Tomamos nota de la Fs y del buffer_size en JACK:
     Fs =            float(jack.get_sample_rate())
