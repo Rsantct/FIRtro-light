f1e493c78a5f7c7520cf3b314f08cc8cc5204bed
Rsantct <rsantct@gmail.com>
2018-04-20 07:38:33 +0000

    Update webcustombutton_8.sh

--- home/firtro/bin/webcustombutton_8.sh
+++ home/firtro/bin/webcustombutton_8.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Ajuste para Spotify (librespot) con Jack solo Playback
+/home/firtro/bin_custom/ampli_on_off.sh
 
-/home/firtro/bin_custom/restart_jack.sh -jp=2048 -jn=3 -bflen=8192 -jP
-/home/firtro/bin/control level_add 0
-/home/firtro/bin/control input none
