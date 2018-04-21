b9c4d28ebea357f3db3983d32863ef0ae7d62c81
Rsantct <rsantct@gmail.com>
2018-04-21 13:38:03 +0200

    Update ampli.sh

--- home/firtro/bin_custom/ampli.sh
+++ home/firtro/bin_custom/ampli.sh
@@ -13,22 +13,30 @@
 # Si no se pasa parámetro, se mostrará el estado del relé al final de este script.
 
 # NOTA: se puede usar ssh para ordenar la ejecución de usbrelay en una máquina remota.
-# Se deja dos lineas comentadas más abajo.
+# Se dejan dos lineas comentadas más abajo.
 
+# Si se pasa parámetro on/off se ejecuta:
 if [[ $1 ]]; then
     tmp="BITFT_1=0"
     if [[ $1 == "on" || $1 == "1" ]]; then
         tmp="BITFT_1=1"
     fi
     cmd="$HOME/bin_custom/usbrelay "$tmp
+    # opc.1 para ejecución en local:
     $cmd
-    #ssh pi@rpi3clac $cmd
+    # opc.2 para ejecución en máquina remota:
+    #ssh firtro@remote_addr $cmd
 fi
-# Vemos el estado del relé.
+
+# Consultamos el estado del relé:
 cmd="$HOME/bin_custom/usbrelay"
+# opc.1 para ejecución en local:
 eval $($cmd)
+# opc.2 para ejecución en máquina remota:
 #eval $(ssh pi@rpi3clac $cmd)
-# EL resultado son variables BITFT_1=x y BITFT_2=y, con x,y in (0,1)
+
+# Mostramos el resultado: que son las variables 
+#   BITFT_1=x y BITFT_2=y, con x,y in (0,1)
 # que son leidas por el shell.
 # En nuestro caso nos fijamos solo en el rele 1 (BITFT_1)
 if [[ $BITFT_1 == "1" ]]; then
