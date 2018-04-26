bfdc5febfd671d88ffdd1bb3d511e3b2cfa6593f
Rsantct <rsantct@gmail.com>
2018-04-26 18:40:27 +0200

    Update ver_temperaturas.sh

--- home/firtro/bin_custom/ver_temperaturas.sh
+++ home/firtro/bin_custom/ver_temperaturas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for zone in /sys/class/thermal/* ; do
+for zone in /sys/class/thermal/t* ; do
     #cat /sys/class/thermal/thermal_zone0/temp | sed 's/.\{3\}$/.&/'
     temp=$(cat $zone/temp | sed 's/.\{3\}$/.&/')
     echo $( basename $zone ) $temp
