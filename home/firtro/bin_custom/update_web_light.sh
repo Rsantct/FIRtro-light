9576cef5453b35f39179b40858a9c6d3b7cb6c71
Rsantct <rsantct@gmail.com>
2018-07-31 16:26:41 +0200

    Create update_web_light.sh

---
+++ home/firtro/bin_custom/update_web_light.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Cutrescript para actualizar la web de control con la página simplificada
+
+cd /home/firtro/www
+mv index.php www/index.php.old
+mv js/functions.js www/js/functions.js.old
+mv php/functions.php www/php/functions.php.old
+wget https://raw.githubusercontent.com/Rsantct/FIRtro-light/master/home/firtro/www/index.php
+cd js
+wget https://raw.githubusercontent.com/Rsantct/FIRtro-light/master/home/firtro/www/js/functions.js
+cd ../php
+wget https://raw.githubusercontent.com/Rsantct/FIRtro-light/master/home/firtro/www/php/functions.php
+
+cd ~/bin
+rm -f poweroff_reboot.sh
+wget https://raw.githubusercontent.com/Rsantct/FIRtro-light/master/home/firtro/bin/poweroff_reboot.sh
+chmod +x poweroff_reboot.sh
+cd /home/firtro
+
+# Nueva linea en www/config/config.ini
+nueva='poweroff_reboot_button = "reboot"'
+yaesta=$(grep poweroff_reboot_button www/config/config.ini)
+if [ ! "$yaesta" ] ; then
+    echo $nueva >> www/config/config.ini
+fi
+echo "www/config/config.ini:"
+grep poweroff_reboot_button www/config/config.ini
