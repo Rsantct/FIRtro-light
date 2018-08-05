a7e79beac291b3ac13dcc5a9e227cabfa8056ff4
Rsantct <rsantct@gmail.com>
2018-08-05 18:14:58 +0200

    Create instala_FIRtro-light.sh

---
+++ home/firtro/bin_custom/instala_FIRtro-light.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Cutrescript para instalar los scripts bin_custom/* de FIRtro-light
+
+# Baja la distro FIRtro-light
+cd /home/firtro/tmp
+rm -f master.zip
+wget https://github.com/Rsantct/FIRtro-light/archive/master.zip
+unzip master.zip
+rm -f master.zip
+
+# Copia los scripts de bin_custom y las url de emisoras de radio
+cd /home/firtro/
+cp tmp/FIRtro-light-master/home/firtro/bin_custom/*   bin_custom/
+cp tmp/FIRtro-light-master/home/firtro/audio/radio_urls   audio/
