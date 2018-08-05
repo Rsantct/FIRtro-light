e080be441b44ec131d805855fc0faa4211d854b4
Rsantct <rsantct@gmail.com>
2018-08-05 18:50:03 +0200

    Update instala_FIRtro-light.sh

--- home/firtro/bin_custom/instala_FIRtro-light.sh
+++ home/firtro/bin_custom/instala_FIRtro-light.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Cutrescript para instalar los scripts bin_custom/* de FIRtro-light
+# Cutrescript para instalar los archivos de FIRtro-light
 
 # Baja la distro FIRtro-light
 cd /home/firtro/tmp
@@ -9,12 +9,15 @@ wget https://github.com/Rsantct/FIRtro-light/archive/master.zip
 rm -rf FIRtro-light-master
 unzip master.zip
 rm -f master.zip
-
-# Copia los scripts de bin_custom y el archivo con las url de emisoras de radio
 cd /home/firtro/
-cp tmp/FIRtro-light-master/home/firtro/bin_custom/*   bin_custom/
+
+# Los scripts de bin_custom
+cp tmp/FIRtro-light-master/home/firtro/bin_custom/*  bin_custom/
 chmod +x bin_custom/*
-cp tmp/FIRtro-light-master/home/firtro/audio/radio_urls   audio/
+
+# El archivo con las url de emisoras de radio, y plantillas para 4 y 8 paramétricos en Ecasound
+cp tmp/FIRtro-light-master/home/firtro/audio/radio_urls audio/
+cp tmp/FIRtro-light-master/home/firtro/audio/PEQ*.      audio/
 
 # Actualiza la web de control con la página de control simplificada:
 sh tmp/FIRtro-light-master/home/firtro/bin_custom/update_web_light.sh
