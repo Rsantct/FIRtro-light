d428b4176075867deaa462062cbd3bd9d546664a
Rsantct <rsantct@gmail.com>
2018-08-05 18:40:07 +0200

    Update instala_FIRtro-light.sh

--- home/firtro/bin_custom/instala_FIRtro-light.sh
+++ home/firtro/bin_custom/instala_FIRtro-light.sh
@@ -10,8 +10,11 @@ rm -rf FIRtro-light-master
 unzip master.zip
 rm -f master.zip
 
-# Copia los scripts de bin_custom y las url de emisoras de radio
+# Copia los scripts de bin_custom y el archivo con las url de emisoras de radio
 cd /home/firtro/
 cp tmp/FIRtro-light-master/home/firtro/bin_custom/*   bin_custom/
 chmod +x bin_custom/*
 cp tmp/FIRtro-light-master/home/firtro/audio/radio_urls   audio/
+
+# Actualiza la web de control con la página de control simplificada:
+sh tmp/FIRtro-light-master/home/firtro/bin_custom/update_web_light.sh
